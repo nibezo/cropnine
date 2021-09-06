f874beac3c6512b4b88c5e367c86be36a58f0c2c
nibezo <nibezo.cs@gmail.com>
2021-09-07 01:56:34 +0300

    feat: add vkwave, logging and greetings fot an user

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+import vktoken
+import logging
 from cropping import cropping, check_size
 from PIL import Image
+from vkwave.bots import SimpleLongPollBot
+
+bot = SimpleLongPollBot(tokens=vktoken.token, group_id=206983513)
+logging.basicConfig(level="DEBUG")
+
+
+@bot.message_handler(bot.text_contains_filter(["Start", "Начало", "Начать", "Старт"]))
+async def start(event: bot.SimpleBotEvent):
+    await event.answer("Привет, я умею только одну вещь - обрезать фото на 9 равных частей. \n\n Убедись, "
+                       "что присылаешь мне квадратное фото, я могу обрезать только такие. \n\n Присылай мне фото 📸")
+
 
 if check_size(Image.open("image.png")):
     # call cropping function
     cropping("image.png")
 else:
     print("Please, crop the image to a square.")
+
+bot.run_forever()
